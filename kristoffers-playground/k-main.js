e9942da2d597e9be8fe495d944c0d97a1da01a01
BouncyPantaloons <kristoffer.stinnes@gmail.com>
2022-01-14 12:18:33 -0800

    hi

--- kristoffers-playground/k-main.js
+++ kristoffers-playground/k-main.js
@@ -3,3 +3,21 @@ var click_effect = new Audio("click.wav");
 function OnButtonClick() {
     click_effect.play();
 }
+
+function OnMouseOver(element) {
+    elements = document.getElementsByClassName(element.className[0]);
+    
+    for(let i = 0; i< elements.length; i++) {
+        elements[i].style.boxShadow = "0px 15px currentColor";
+        elements[i].style.marginTop = "-5px";
+    }
+}
+
+function OnMouseLeave(element) {
+    elements = document.getElementsByClassName(element.className[0]);
+    
+    for(let i = 0; i< elements.length; i++) {
+        elements[i].style.boxShadow = "0px 10px currentColor";
+        elements[i].style.marginTop = "0px";
+    }
+}
